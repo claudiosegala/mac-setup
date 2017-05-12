8dcd55e9ff9cec7d39ee2e605885e7722c0e5725
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-05-12 12:02:22 -0300

    Add files via upload

---
+++ setup_git.sh
@@ -0,0 +1,12 @@
+mkdir git
+git clone https://github.com/rafael-fernandes/projeto_zulp.git
+git clone https://github.com/claudiosegala/numerical-analysis-algorithms.git
+git clone https://github.com/claudiosegala/brew_essencials.git
+git clone https://github.com/claudiosegala/blog.git
+git clone https://github.com/claudiosegala/koans.git
+git clone https://github.com/claudiosegala/TEP.git
+git clone https://github.com/claudiosegala/games.git
+git clone https://github.com/claudiosegala/uva-node
+cd uva-node
+npm install && npm start
+cd
