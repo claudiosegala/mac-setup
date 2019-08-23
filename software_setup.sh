9e09f9838f864ac4d0412e92aa150624fa74f97d
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-08-22 22:25:15 -0300

    making it minimal

--- software_setup.sh
+++ software_setup.sh
@@ -1,289 +1,73 @@
-#############################################################################################
-#Set up folders
-#############################################################################################
+# Set git projects folder
+cd && mkdir Projects
 
-	# Set Git Projects folder
-	cd
-	mkdir Projects
+# Get a brew on
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#############################################################################################
-#Brew Setup
-#############################################################################################
+# Get some repositories
+brew tap homebrew/code
+brew tap homebrew/services
+brew tap caskroom/cask
+brew tap caskroom/versions
 
-	# Get a brew on
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install
+brew cask install google-chrome
+brew cask install google-backup-and-sync
+brew cask install android-file-transfer
+brew cask install vlc
+brew cask install spotify
+brew cask install the-unarchiver
+brew cask install appcleaner
+brew cask install skype
+brew cask install slack
+brew cask install whatsapp
+brew cask install telegram
+brew cask install cakebrew
 
-	# Get some tap repositories
-	brew tap caskroom/versions
-	brew tap homebrew/services
-	brew tap homebrew/core
-	brew tap caskroom/cask
+brew install repo # for android development
+brew install git git-crypt # git-crypt make support for automatic encryption on pushing to git
+brew install gcc
+brew install cunit
+brew install libyaml # YAML 1.1 parser and emitter written in C
+brew install readline # library that provides a set of functions for use by applications that allow users to edit command lines as they are typed in
+brew install htop # better 'top'
+brew install openssh # Open SSH
+brew install ssh-copy-id # The main OpenSSH page
+brew install vim --with-override-system-vi
+brew install bash # Latest version of bash
+brew install bash-completion # Excelent escape command completion for bash
+brew install cmake # to generate makefile
+brew install make # for makefile
+brew cask install texmaker # Latex
+brew install go
+brew install node npm
+brew install mysql # sqlite postgresql
+brew install python3 pip3
+pip3 install virtualenv virtualenvwrapper
+#brew install docker-machine
 
-	# Set up
-	brew update
-	brew cleanup
-	brew install repo
-	echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.bash_profile
-	source ~/.bash_profile
-	brew doctor
+# Config paths
+echo "
+# repo
+export PATH="/usr/local/sbin:$PATH"
 
-#############################################################################################
-#Git Setup
-#############################################################################################
-
-	# Git Install
-	brew install git
-	git --version
-	# git config --global user.name "Your name"
-	# git config --global user.email "Your Email"
-	git config -l
-	brew install git-crypt 	#to support automatic encryption on pushing to git
-
-#############################################################################################
-#Command Line Setup
-#############################################################################################
-
-	# GCC
-	brew install gcc
-
-	# CUnit
-	brew install cunit
-
-	# Install Latex
-	brew cask install texmaker
-
-	# Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages
-	brew install autoconf
-
-	# Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards
-	brew install automake
-
-	# the GNU `gettext' utilities are a set of tools that provides a framework to help other GNU packages produce multi-lingual messages
-	brew install gettext
-
-	# Library for manipulating sets and relations of integer points bounded by linear constraints
-	brew install isl
-
-	# Libgpg-error is a small library that defines common error values for all GnuPG components
-	brew install libgpg-error
-
-	# YAML 1.1 parser and emitter written in C
-	# brew install libyaml
-
-	# Library that provides a set of functions for use by applications that allow users to edit command lines as they are typed in
-	brew install readline
-
-	# Open SSH
-	brew install openssh
-
-	# The main OpenSSH page
-	brew install ssh-copy-id
-
-	# Supports the awesome mmd formatting system
-	# brew install multimarkdown
-
-	# Vim
-	brew install vim --with-override-system-vi
-
-	# Gives you v4, a more modern version (note you need to add it to approved shells and use chsh to switch to it
-	brew install bash
-
-	# Excellent escape command completion for bash
-	brew install bash-completion
-
-	# gives you network bandwidth measurement
-	brew install speedtest_cli
-
-	#for simple high speed file transfer
-	brew install wget
-
-	# Make to unite libraries
-	brew install cmake
-	brew install make
-
-	# Heroku
-	# brew install heroku
-	# heroku --version
-	# heroku login
-
-#############################################################################################
-#Programming Languages Setup
-#############################################################################################
-
-	# Ruby
-	brew install ruby
-
-	# Python 3
-	brew install python3 # 3
-	brew install pip3
-	pip3 install virtualenv virtualenvwrapper
-	echo "
-##
-# virtualenv and virtualenvwrapper
-## 
-
+# virtualenv and virtualenvwrapper 
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 source /usr/local/bin/virtualenvwrapper.sh
+" >> ~/.bash_profile
+source ~/.bash_profile
 
-	" >> ~/.bash_profile
-	source ~/.bash_profile
-	#sudo easy_install pip
+# Config git
+git config --global user.name "Claudio Segala Rodrigues Silva Filho"
+git config --global user.email "claudiosegalafilho@gmail.com"
 
-	# Node / NPM
-	brew install node
-	brew install npm
-	brew install yarn
+# Finishing setup
+brew update
+brew cleanup
+brew upgrade
+brew cleanup
+brew doctor
 
-	# Go
-	# brew install go
-
-	# Rust
-	# brew install rust
-
-	# Haskell
-	# brew cask install haskell-platform
-
-	# Java
-	# brew update
-	# brew cask install java
-
-	# SQL
-	brew install mysql # MySQL
-	# brew install sqlite # SQLite
-	# brew install postgresql # PostgresSQL
-
-	# Stylus
-	# npm install stylus -g
-
-	# Elm
-	# brew install elm
-
-	# RVM - Rails
-	# \curl -L https://get.rvm.io | bash -s stable
-	# osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down'
-	# rvm | head -n 1
-	# rvm use ruby --install --default
-	# rails --version
-	# gem install devisegem install rails --no-ri --no-rdoc
-	# gem install compass
-	# gem install jekyll
-	# gem install binding_of_caller
-	# gem install devise
-	# gem install cancancan
-	# gem install cucumber-rails
-	# gem install rspec-rails
-	# gem install capybara
-	# gem install simplecov
-	# gem install rubocop
-	# gem install rolify
-	# gem install factory_girl
-	# gem install factory_girl_rails
-	# gem install pry-rails
-	# gem install pry-byebug
-	# gem install better_errors
-	# gem install rspec-rails
-	# gem install carrierwave
-	# gem install mini_magick
-	# gem install sidekiq
-	# gem install sucker_punch
-	# gem install simple_form
-	# gem install elasticsearch
-	# gem install brakeman
-	# gem install database_cleaner
-	# brew install imagemagick		
-
-	# Elixir
-	# brew install erlang
-	# brew install elixir
-
-	# Phoenix
-	# mix local.hex
-	# mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
-
-#############################################################################################
-#Apps Setup
-#############################################################################################
-
-	# Atom
-	# brew cask install atom
-
-	# Aquamacs
-	# brew cask install aquamacs
-
-	# Google Chrome
-	brew cask install google-chrome
-
-	# Chromium
-	# brew cask install chromium
-
-	# Firefox
-	# brew cask install firefox
-
-	# Google Backup and Sync
-	brew cask install google-backup-and-sync
-
-	# Android File Transfer
-	brew cask install android-file-transfer
-
-	# Dropbox
-	# brew cask install dropbox
-
-	# VLC
-	brew cask install vlc
-
-	# Spotify
-	brew cask install spotify
-
-	# Unarquiver
-	brew cask install the-unarchiver
-
-	# AppCleaner
-	brew cask install appcleaner
-
-	# Skype
-	brew cask install skype
-
-	# Slack
-	brew cask install slack
-
-	# Whatsapp
-	brew cask install whatsapp
-
-	# Telegram
-	brew cask install telegram
-
-	# Cakebrew
-	# brew cask install cakebrew
-
-	# Team Viewer
-	# brew cask install teamviewer
-
-	# Gimp
-	# brew cask install gimp
-
-	# Utorrent
-	# brew cask install utorrent
-
-	# Virtualbox
-	# brew cask install virtualbox
-
-	# Docker Machine
-	# brew install docker-machine
-
-#############################################################################################
-#Brew Final Setup
-#############################################################################################
-
-	# Finishing setup
-	brew update
-	brew upgrade
-	brew cleanup
-	brew doctor
-
-#############################################################################################
-#Finish
-#############################################################################################
-
-	# Close Window
-	osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "w" using command down' &&
+# Close Window
+osascript -e 'tell application "System Events" to tell process "Terminal" to keystroke "w" using command down' &&
