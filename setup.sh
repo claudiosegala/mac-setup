d31b0f6ddec1180f5a9a4c423083a54eda66f16a
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2019-08-24 17:47:27 -0300

    more git configs and stuff

--- setup.sh
+++ setup.sh
@@ -25,17 +25,19 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 # Install
-brew cask install google-chrome
-brew cask install google-backup-and-sync
-brew cask install android-file-transfer
-brew cask install vlc
-brew cask install spotify
-brew cask install the-unarchiver
-brew cask install appcleaner
-brew cask install slack
-brew cask install whatsapp
-brew cask install telegram
-brew cask install cakebrew
+brew cask install visual-studio-code # general editor
+brew cask install typora # markdown editor
+brew cask install google-chrome # browser
+brew cask install google-backup-and-sync # backup
+brew cask install whatsapp # communication
+brew cask install telegram # communication
+brew cask install slack # communication
+brew cask install spotify # music
+brew cask install vlc # video player
+brew cask install stremio # streaming
+brew cask install cakebrew # brew GUI
+brew cask install appcleaner # remove apps
+brew cask install android-file-transfer # android manipulation
 
 brew install git git-crypt # git-crypt make support for automatic encryption on pushing to git
 brew install gcc
@@ -59,9 +61,19 @@ cat files/bash_profile_python_env >> ~/.bash_profile
 cat files/bash_profile_go_env >> ~/.bash_profile
 source ~/.bash_profile
 
-# Config git
+# Config git (based on https://gist.github.com/scottnonnenberg/fefa3f65fdb3715d25882f3023b31c29)
 git config --global user.name "Claudio Segala Rodrigues Silva Filho"
 git config --global user.email "claudiosegalafilho@gmail.com"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.ff 'merge --ff-only'
+git config --gloabl alias.undo 'reset --soft HEAD^'
+git config --global alias.stash-all 'stash save --include-untracked'
+git config --global alias.glog 'log --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\''
+#git config --global commit.gpgSign true
+git config --global core.editor "code --wait"
 
 # Config ruby
 \curl -L https://get.rvm.io | bash -s stable
