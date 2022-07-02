fae430b8528168dc0c3f2431c6e923068a8c8a4b
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2022-07-02 15:21:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+# Get Git setup information
+printf '\nWhat name do you want to add on your git commit setup? '
+read -r git_name
+
+printf '\nWhat email do you want to add on your git commit setup? '
+read -r git_email
+
+
 # Set up competitive programming stuff
 mkdir ~/../../usr/local/include/bits && touch ~/../../usr/local/include/bits/stdc++.h
 cat files/stdc++.h >> ~/../../usr/local/include/bits/stdc++.h
@@ -15,55 +23,75 @@ source ~/.bash_profile
 mkdir ~/Projects
 
 # Get a brew on
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "export PATH="/usr/local/sbin:$PATH" # brew" >> ~/.bash_profile && source ~/.bash_profile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Get some repositories
-brew tap homebrew/code
-brew tap homebrew/services
-brew tap caskroom/cask
-brew tap caskroom/versions
+# Install Applications
+brew install --cask visual-studio-code # code editor
+# brew install --cask typora # markdown editor
+brew install --cask whatsapp
+brew install --cask messenger
+brew install --cask telegram
+# brew install --cask slack
+brew install --cask nordvpn # vpn
+brew install --cask stremio # pirate netflix
+brew install --cask spotify # music streaming
+brew install --cask vlc # video player
+brew install --cask google-drive # cloud backup
+# brew install --cask android-file-transfer # android file transfer
+# brew install --cask cakebrew
+# brew install --cask appcleaner
 
-# Install
-brew cask install visual-studio-code # general editor
-brew cask install typora # markdown editor
-brew cask install google-chrome # browser
-brew cask install google-backup-and-sync # backup
-brew cask install whatsapp # communication
-brew cask install telegram # communication
-brew cask install slack # communication
-brew cask install spotify # music
-brew cask install vlc # video player
-brew cask install stremio # streaming
-brew cask install cakebrew # brew GUI
-brew cask install appcleaner # remove apps
-brew cask install android-file-transfer # android manipulation
+# Install Tools
+brew install just
+brew install git
 
-brew install git git-lfs git-crypt # git-crypt make support for automatic encryption on pushing to git
-brew install gcc
-brew install htop # better 'top'
-brew install openssh # Open SSH
-brew install ssh-copy-id # The main OpenSSH page
-brew install vim --with-override-system-vi
-brew install bash # Latest version of bash
-brew install bash-completion # Excelent escape command completion for bash
-brew install make # for makefile
-brew install go
-brew install ruby
-brew install node npm
-brew install mysql # sqlite postgresql
-brew install python3 pip3
-pip3 install virtualenv virtualenvwrapper
-#brew install docker-machine
+# Install Deno
+brew install deno
 
-# Config paths
-cat files/bash_profile_python_env >> ~/.bash_profile
-cat files/bash_profile_go_env >> ~/.bash_profile
-source ~/.bash_profile
+# Install Misc Tools
+# brew install gcc
+# brew install htop # better 'top'
+# brew install openssh # Open SSH
+# brew install ssh-copy-id # The main OpenSSH page
+# brew install vim --with-override-system-vi
+# brew install bash # Latest version of bash
+# brew install bash-completion # Excelent escape command completion for bash
+# brew install make # for makefile
+# brew install mysql # sqlite postgresql
+# brew install docker-machine
+
+# Install Ruby
+# brew install ruby
+# \curl -L https://get.rvm.io | bash -s stable
+# source ~/.rvm/scripts/rvm
+# rvm | head -n 1
+# rvm use ruby --install --default
+# gem install jekyll
+
+# Install JS
+# brew install node npm
+
+# Install Python 
+# brew install python3 pip3
+# pip3 install virtualenv virtualenvwrapper
+# cat files/bash_profile_python_env >> ~/.bash_profile
+
+# Install Go
+# brew install go
+# cat files/bash_profile_go_env >> ~/.bash_profile
+# source ~/.bash_profile
+# mcd ~/Projects\ Golang
+# mkdir bin
+# mkdir src
+# echo "export GOPATH=\"$HOME/Projects Golang\"" >> ~/.bash_profile
 
 # Config git (based on https://gist.github.com/scottnonnenberg/fefa3f65fdb3715d25882f3023b31c29)
-git config --global user.name "Claudio Segala Rodrigues Silva Filho"
-git config --global user.email "claudiosegalafilho@gmail.com"
+gh config set editor "code -w"
+
+git config --global user.name $git_name
+git config --global user.email $git_email
+git config --global core.editor "code --wait"
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
@@ -72,22 +100,8 @@ git config --global alias.ff 'merge --ff-only'
 git config --gloabl alias.undo 'reset --soft HEAD^'
 git config --global alias.stash-all 'stash save --include-untracked'
 git config --global alias.glog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
-#git config --global commit.gpgSign true
-git config --global core.editor "code --wait"
+
 echo .DS_Store >> ~/.gitignore_global
-
-# Config ruby
-\curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm | head -n 1
-rvm use ruby --install --default
-gem install jekyll
-
-# Config go
-mcd ~/Projects\ Golang
-mkdir bin
-mkdir src
-echo "export GOPATH=\"$HOME/Projects Golang\"" >> ~/.bash_profile
 
 # Finishing setup
 brew doctor
